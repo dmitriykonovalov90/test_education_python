bfef1a6ed72bba86867efb11c9ba32df04f41521
dmitriykonovalov90 <martovskiy07kot@mail.ru>
2021-02-27 15:57:49 +0300

    Коновалов, ДЗ2, задача №1

--- konovalov_dz2_task1.py
+++ konovalov_dz2_task1.py
@@ -1,1 +1,8 @@
-hello
+result1 = 15 * 3
+result2 = 15 / 3
+result3 = 15 // 2
+result4 = 15 ** 2
+list_results = [result1, result2, result3, result4]
+
+for key, type_values in enumerate(list_results, 1):
+    print("Тип", key, "результата выражения: ", type(type_values))
